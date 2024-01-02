616d583a709ef29682f5a7dd81f0ceb2fb0a339f
mitchmcnaughton <141999182+mitchmcnaughton@users.noreply.github.com>
2024-01-03 00:34:42 +1100

    made get, getall, delte and update functions for user

---
+++ controllers/thoughtController.js
@@ -0,0 +1,7 @@
+const { Thought, User } = require ('../models');
+
+module.exports = {
+
+
+
+}
---
+++ controllers/userController.js
@@ -0,0 +1,87 @@
+const { Thought, User } = require ('../models');
+
+module.exports = {
+
+    //get all users
+
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    //get one user
+
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.UserId })
+                
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that id' });
+            }
+
+            res.json(user);
+
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    //create a user
+
+    async createUser(req, res) {
+        try {
+            const UserData = await User.create(req.body);
+            res.json(UserData);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    
+    },
+
+    //delete user
+
+    async createUser(req, res) {
+        try {
+            const UserData = await User.findByIdAndDelete({ _id: req.params.UserId });
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that id' });
+            }
+            res.json({ message: 'Deleted User'});
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    
+    },
+
+    //update user
+
+    async createUser(req, res) {
+        try {
+            const UserData = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true}
+                );
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that id' });
+            }
+            res.json(UserData);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    
+    },
+
+
+
+
+};
